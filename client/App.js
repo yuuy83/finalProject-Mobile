c38004af7f41dbf9eb4ad269f7abcaf1e2bb0e30
yuuy83 <nghiawapro2@gmail.com>
2024-12-10 15:26:27 +0700

    test App

--- client/App.js
+++ client/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Navigation from "./navigation";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
